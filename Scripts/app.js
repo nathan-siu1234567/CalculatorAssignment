ade62d84bb0fbcc4642754a731ab862d15141957
nathan-siu <nathansiu9730@gmail.com>
2017-01-31 16:06:51 -0500

    create displayScreen function

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
-    $("#number").on('click',function(){
-        alert("clicked");
+$(document).ready(function(){
+
+    var result =0;
+    var oldEntry =0;
+    var operator = null;
+    var currentEntry =0;
+    //displayScreen(result);
+
+    //number click function
+    $('.numberButton').on('click',function(){
+        var value = $(this).html();
+        console.log(value);
+
+        if(currentEntry === '0')
+        {
+            currentEntry = value;
+        }
+        displayScreen(currentEntry);
+
     });
+
+
 });
+//update screen function
+displayScreen = function(outputVal) {
+  var outputVal = outputVal.toString();
+  $('.screen').html(outputVal.substring(0, 10));
+};
